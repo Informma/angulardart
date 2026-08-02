1b9c98c6dd9215ab5bc68e06b0be68e9ce94167a
Guy-Yann VECTOL <flutterdocteur@gmail.com>
2026-08-02 20:07:23 +0200

    chore: stabilize compiler APIs and update dependencies for pub.dev score

--- angular/angular_compiler/lib/v1/cli.dart
+++ angular/angular_compiler/lib/v1/cli.dart
@@ -3,12 +3,7 @@
 /// Utilities and classes here are not directly related to the compilation
 /// pipeline, but instead are used to manage the process, logging, exception
 /// handling, and other related functionality.
-///
-/// **NOTE**: This is not an externally stable API. Use at your own risk.
-@experimental
 library angular_compiler.cli;
-
-import 'package:meta/meta.dart';
 
 export 'src/angular_compiler/cli/builder.dart';
 export 'src/angular_compiler/cli/flags.dart';
